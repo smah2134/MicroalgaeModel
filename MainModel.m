995aa4864eb548fea0c12aabfe15dcf80823465c
smah2134 <116727092+smah2134@users.noreply.github.com>
2022-11-01 07:47:08 +0100

    Add files via upload

---
+++ MainModel.m
@@ -0,0 +1,109 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
+%       Main model for the pridiction of microalgal growth
+%
+%
+%
+%
+%       
+%
+%
+%
+%
+%
+%           Copyright 2022: Alexander Hofmann
+%
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+clear
+close all
+
+
+%% Load Environmental Data
+
+load 'filename'                      %% Change to file name
+LightPAR= ;                          %% Specify PAR data
+LightGlobal= ;                       %% Specify global radiation data
+AmbientTemperature= ;                %% Specify ambient temperature data
+WaterTemperature = ;                 %% Specify water temperature data
+ReactorTemperature= ;                %% Specify reactor temperature data
+datetime =      ;                %% Date time     
+
+
+
+
+%% Set initial Conditions
+
+
+
+X_start = 0.5;                                                                       %Initial microalgae concentration (g/L)
+t_total = 11;                                                                        %Total simulation time (d) 
+timestep = 0.5/(24*60);                                                              %Time step size (d)
+timestep_s =timestep_AH1*24*3600;                                                    %Time step size (s)               
+timesteps_total = t_total/timestep;                                                  %Total time steps                                                                                
+X(1) = X_start_AH1;                                                                  %Microalgae concentration at t=1
+    
+
+
+
+%% Load geometry
+
+[R,dx,dtheta,dthetaR,L,x,rho,dx1,Rv,Rstep,theta,V,x_i,y_i,V_total,thetasteps] = ReactorGeometry_Tube;                                                    %Load tube geometry function
+
+
+
+for t=1:1:timesteps_total
+
+
+    %% Read Light Data
+
+    %Read incident light intensity data for specific time step
+    I_0(t) = LightPAR(t,1);                                  %Incident light (PAR) intensity (micromol/m²s)
+
+
+
+    %% Read Temperature Data
+    %Read reactor temperature data for specific stime step
+    T_R(t) = ReactorTemperature(t,1);   %Measured reactor temperature (°C)
+
+
+
+    %% Light Distribution Model
+        
+       % Beer-Lambert Model
+     [I]=LightModel_Tube_BeerLambert(X(t),I_0(t),R,x_i,y_i);
+
+
+    %% Growth Kinetics Model
+
+
+        [mu_max,mu,X_dv] = GrowthModel_simpleMonod_CTMI_Resp(T_R(t),I,I_0(t),timestep,X(t));
+
+
+
+        Biomass_abs = X_dv.*V;                                  % New biomass of each volume element
+        X(t+1) =  sum(Biomass_abs,'all')./V_total;          % New biomass concentration of each volume element
+        time(t) = t.*timestep;                          % Real time
+        timeO(t+1) = t.*timestep;                       % Real time with off-set
+
+
+end
+
+
+%% Plot results
+
+sz = 10;
+
+figure(1)
+plot(timeO,X, 'black')
+legend({'Simulation'},'Location','northwest','Orientation','vertical')
+xlabel('Cultivation time (d)')
+ylabel('Biomass concentration (g/L)')
