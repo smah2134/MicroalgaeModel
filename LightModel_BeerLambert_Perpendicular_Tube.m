cb9536ba7e0f817e22175204b607e9b16ec48260
smah2134 <116727092+smah2134@users.noreply.github.com>
2022-11-01 19:56:36 +0100

    Add files via upload

--- LightModel_BeerLambert_Perpendicular_Tube.m
+++ LightModel_BeerLambert_Perpendicular_Tube.m
@@ -1,9 +1,11 @@
 function [I] = LightModel_BeerLambert_Perpendicular_Tube(X,I_0,R,x_i,y_i)
 
+%% Beer-Lambert law 
+
+%Assumptions: Solar irradiation is perpendicular to the reactor surface
 
 
-
-k = 80;                                                  %% Absorption/Extinction coefficient
+k = 80;                                                  %% Absorption/Extinction coefficient change depending on specific optical properties of the microalgae
 
 I = I_0.*exp(-k*X*((sqrt((R^2)-(x_i.^2))-y_i)/1000));    %% Light intensity at every point in the tube. X is the biomass concentration, sqrt((R^2 - x^2) y) is the mono-dimensional light path length in y-direction
 
