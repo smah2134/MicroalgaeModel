59201f26e04e79f66fc8a54def72bed185d0e216
smah2134 <116727092+smah2134@users.noreply.github.com>
2022-11-02 12:52:26 +0100

    Add files via upload

--- MainModel.m
+++ MainModel.m
@@ -3,16 +3,18 @@
 %
 %       Main model for the pridiction of microalgal growth
 %
+%           Author: Alexander Hofmann
 %
-%
-%
+%           This model couples the light and temperature sub-models to the
+%           kinetic growth sub-model to calculate the microalgae concentration
+%           for eacht time step.
 %       
 %
 %
 %
 %
 %
-%           Copyright 2022: Alexander Hofmann
+%           Copyright 2022 Alexander Hofmann
 %
 %
 %
@@ -29,8 +31,8 @@ close all
 
 %% Load Environmental Data
 
-load 'filename'                      %% Change to file name
-LightPAR= ;                          %% Specify PAR data
+load 'filename'                      %% Change to file name (e.g. matlab table data format)
+LightPAR= ;                          %% Specify PAR data (micromol/m²s)
 LightGlobal= ;                       %% Specify global radiation data
 AmbientTemperature= ;                %% Specify ambient temperature data
 WaterTemperature = ;                 %% Specify water temperature data
@@ -47,16 +49,18 @@ datetime =      ;                %% Date time
 X_start = 0.5;                                                                       %Initial microalgae concentration (g/L)
 t_total = 11;                                                                        %Total simulation time (d) 
 timestep = 0.5/(24*60);                                                              %Time step size (d)
-timestep_s =timestep_AH1*24*3600;                                                    %Time step size (s)               
+timestep_s =timestep*24*3600;                                                    %Time step size (s)               
 timesteps_total = t_total/timestep;                                                  %Total time steps                                                                                
 X(1) = X_start_AH1;                                                                  %Microalgae concentration at t=1
     
 
+immersion = 0.5;                                                                    %Immersion level, possible values: 0.5, 0.25, 0
+TReactorOld = ReactorTemperature(1);                                                %Start temperature for reactor temperature simulation
 
 
 %% Load geometry
 
-[R,dx,dtheta,dthetaR,L,x,rho,dx1,Rv,Rstep,theta,V,x_i,y_i,V_total,thetasteps] = ReactorGeometry_Tube;                                                    %Load tube geometry function
+[R,V,x_i,y_i,V_total] = ReactorGeometry_Tube;                                                    %Load tube geometry function
 
 
 
@@ -75,24 +79,28 @@ for t=1:1:timesteps_total
     T_R(t) = ReactorTemperature(t,1);   %Measured reactor temperature (°C)
 
 
+    %Reactor temperature simulation
 
-    %% Light Distribution Model
+    [T_Rsim(t+1), TReactorOld(t+1)] = ReactorTempSimMain(immersion,LightGlobal(t),AmbientTemperature(t),WaterTemperature(t),TReactorOld(t));
+
+    %% Light Distribution Model (comment/uncomment for use)
         
        % Beer-Lambert Model
-     [I]=LightModel_Tube_BeerLambert(X(t),I_0(t),R,x_i,y_i);
+        [I]=LightModel_BeerLambert_Perpendicular_Tube(X(t),I_0(t),R,x_i,y_i);  %%Light intensity at every grid point in micromol/m²s
 
 
-    %% Growth Kinetics Model
+
+    %% Growth Kinetics Model (comment/uncomment for use)
 
 
-        [mu_max,mu,X_dv] = GrowthModel_simpleMonod_CTMI_Resp(T_R(t),I,I_0(t),timestep,X(t));
+        [mu_max,mu,X_dv] = GrowthModel_simpleMonod_CTMI_Resp(T_R(t),I,I_0(t),timestep,X(t));  %%Monod model combined with cardinal temperature model with inflexion. change T_R to T_Rsim for the simulated temperature
 
 
 
         Biomass_abs = X_dv.*V;                                  % New biomass of each volume element
-        X(t+1) =  sum(Biomass_abs,'all')./V_total;          % New biomass concentration of each volume element
-        time(t) = t.*timestep;                          % Real time
-        timeO(t+1) = t.*timestep;                       % Real time with off-set
+        X(t+1) =  sum(Biomass_abs,'all')./V_total;              % New biomass concentration of each volume element
+        time(t) = t.*timestep;                                  % Real time
+        timeO(t+1) = t.*timestep;                               % Real time with off-set
 
 
 end
